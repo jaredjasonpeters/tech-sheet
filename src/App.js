46117514f4da4e5e3106f24ca76c6a0edc2a27c4
Jared J. Peters <jjpeters@dlfna.com>
2018-08-28 09:58:05 -0700

    added create new Techsheet button, added toggling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {TechSheetProvider} from './components/providers/providers'
 import DataEntry from './components/data_entry';
 import './App.css';
 import { Context } from './components/contexts/contexts';
+import { SubmitButton, Wrapper } from './components/styled/styled'
 
 
 
@@ -13,12 +14,33 @@ import { Context } from './components/contexts/contexts';
 
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      display_form: false,
+    }
+    this.showForm = this.showForm.bind(this)
+  }
+    
+    showForm() {
+      this.setState((prevState) => {
+        var newState = Object.assign(prevState)
+        newState['display_form'] = !prevState['display_form']
+        return newState
+      })
+    }
+  
   render() {
     return (
       <TechSheetProvider>
       <div className="App">
         <Header companyName='DLF Pickseed' companyLogo='https://www.dlfpickseed.com/Files/Images/DLF_Pickseed_USA/Logos/DLF_Pickseed_Logo_230x93.png' />
-        <DataEntry />
+        {!this.state.display_form && 
+          <Wrapper justify="center">
+            <SubmitButton submit onClick={this.showForm}>Create New Techsheet</SubmitButton>
+          </Wrapper>
+        }
+        {this.state.display_form && <DataEntry />}
         {/* <Techsheet /> */}
         <Context.Consumer>
         {(context)=> (console.log(context.state.form_data))}
