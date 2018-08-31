5c874116d10f2ea35a9cb0dcabc9066ca53b180f
Jared Peters <jjpeters@dlfna.com>
2018-08-31 00:36:28 -0700

    style changes to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import styled, { css } from 'styled-components'
 import { Wrapper } from './styled/styled'
+import { Context } from './contexts/contexts'
 import UserPanel from './user_panel'
 import StyleSwitcher from './style_switcher'
+import { Colors } from '../utils/utils'
 
 const Header = (props) => {
     return (
-        <Wrapper width="100%" row="row" bg-color="white">
-            <CompanyHeader className="App-header">
-                <img src={props.companyLogo} className="App-logo" alt="logo" />
-                <h1 className="App-title">{props.companyName} Tech Sheets</h1>
-            </CompanyHeader>
-            <StyleSwitcher />
-            <UserPanel />
-        </Wrapper>
+        <Context.Consumer>
+            {context => (
+
+                <Wrapper width="100%" row="row" bg-color="white">
+                    <CompanyHeader className="App-header" theme={Colors[context.state.theme_style]}>
+                        <img src={props.companyLogo} className="App-logo" alt="logo" />
+                        <h1 className="App-title">{props.companyName} Tech Sheets</h1>
+                    </CompanyHeader>
+                    <StyleSwitcher />
+                    <UserPanel />
+                </Wrapper>
+            )}
+        </Context.Consumer>
     )
 }
 
@@ -24,11 +31,11 @@ const CompanyHeader = styled.header`
         flex-grow: 5;
         max-width: 30%;
         align-self: flex-start;
-        background-color: #222;
+        background-color: ${props => props.theme || '#222'};
         height: 10%;
         padding: 20px 0 0px 50px;
         color: white;
-        border-radius: 0px 0px 15px 0px;
+        border-radius: 0px 0px 0px 0px;
     `
 
 export default Header
