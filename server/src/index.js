e0dbf028078053a78e5983342ba1fa57074b7d10
Jared Peters <jjpeters@dlfna.com>
2018-09-05 23:44:08 -0700

    worked on backend

--- server/src/index.js
+++ server/src/index.js
@@ -1,52 +1,29 @@
 const { GraphQLServer } = require('graphql-yoga')
-const { links } = require('../seed')
 const { findById } = require('../helpers')
-
-let idCount = links.length
-
+const { Prisma } = require('prisma-binding')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const AuthPayload = require('./resolvers/AuthPayload')
 
 
 const resolvers = {
-    Query: {
-        info: () => `This is the API`,
-        feed: () => links,
-        link: (root, args) => {
-            return findById(args, links, (link)=>link)
-        }
-    },
-    Mutation: {
-        post: (root, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url,
-            }
-            links.push(link)
-            return link
-        },
-        updateLink: (root, args) => {
-            return findById(args, links, link => {
-                link.description = args.description || link.description
-                link.url = args.url || link.url
-                return link
-            })
-        },
-        deleteLink: (root, args) => {
-            return findById(args, links, (link,index,links) => {
-                return links.pop(links[index])
-            })
-        }
-    },
-    Link: {
-        id: (root) => root.id,
-        description: (root) => root.description,
-        url: (root) => root.url,
-    }
+    Query,
+    Mutation,
+    AuthPayload,
 }
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
+    context: req => ({
+        ...req,
+        db: new Prisma({
+            typeDefs: 'src/generated/prisma.graphql',
+            endpoint: 'https://us1.prisma.sh/jared-peters-2fd802/dlf_pickseed/dev',
+            secret: 'Oregonseed2018',
+            debug: true,
+        }),
+    }),
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
