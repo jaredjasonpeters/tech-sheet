5d8cf2ac2e3401a845a340f47e798026d8b726e2
Jared J. Peters <jjpeters@dlfna.com>
2018-09-11 15:35:38 -0700

    started messing around with Prisma Beta for DB

--- server/src/index.js
+++ server/src/index.js
@@ -4,12 +4,14 @@ const { Prisma } = require('prisma-binding')
 const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const AuthPayload = require('./resolvers/AuthPayload')
+const Subscription = require('./resolvers/Subscription')
 
 
 const resolvers = {
     Query,
     Mutation,
     AuthPayload,
+    Subscription,
 }
 
 const server = new GraphQLServer({
