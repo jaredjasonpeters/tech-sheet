101899735ff36018335f3f615f3c3f3e8ef27e96
Jared J. Peters <jjpeters@dlfna.com>
2018-11-29 18:39:40 -0800

    changed Route in PrivateRoute.js to render child

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,27 +1,21 @@
-import React from 'react';
-import {
-    Route,
-    Redirect
-} from 'react-router-dom'
-
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
-    <Route
-      {...rest}
-      render={props => 
-        rest.loginContext.state.isAuthenticated ? (
-          <Component {...rest} />
-        ) : (
-          <Redirect
-            to={{
-              pathname: "/",
-              state: { from: props.location }
-            }}
-          />
-        )
+  <Route {...rest}>
+    {props =>
+      rest.loginContext.state.isAuthenticated ? (
+        <Component {...rest} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/",
+            state: { from: props.location }
+          }}
+        />
+      )
     }
-    />
-  );
-  
+  </Route>
+);
 
-export default  PrivateRoute
+export default PrivateRoute;
